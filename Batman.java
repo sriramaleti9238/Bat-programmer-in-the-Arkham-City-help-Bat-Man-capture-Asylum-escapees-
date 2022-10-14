955cc01636457851ac4c1759d34186a0ca5f6733
sriramaleti9238 <106054174+sriramaleti9238@users.noreply.github.com>
2022-10-14 21:58:45 +0530

    Create Batman.java

---
+++ Batman.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class Batman {
+    public static int findNumOfStepsRequired(int x, int y)
+    {
+        return x*y;
+    }
+    public static void main(String args[])
+    {
+        int z=findNumOfStepsRequired(3, 10);
+        System.out.println(z);
+    }
+    
+}
